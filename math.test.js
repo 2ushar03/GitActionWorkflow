cbf362f7581e730d33f8bbd2ef947f65aefa15bc
2ushar03 <tusharbpatil5784@gmail.com>
2024-10-14 22:46:38 +0530

    First Commit

---
+++ math.test.js
@@ -0,0 +1,19 @@
+const { add, subtract, divide, multiply, percentage } = require("./math");
+
+describe("Math.js tests", () => {
+  it("should test  add  ", () => {
+    expect(add(4, 4)).toBe(8);
+  });
+  it("Should test subtract", () => {
+    expect(subtract(4,2)).toBe(2);
+  });
+  it("Should test divide", () => {
+    expect(divide(20,2)).toBe(10);
+  });
+  it("Should test multiply", () => {
+    expect(multiply(30,2)).toBe(60);
+  });
+  it("Should test percentage",()=>{
+    expect(percentage(4,1)).toBe(80);
+  });
+});
